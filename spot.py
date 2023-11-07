4a80713d438ae52360a37d6a83ae6b27601899e3
KeKeiGenkai <99686764+KeKeiGenkai@users.noreply.github.com>
2023-11-07 22:32:46 +0300

    Update spot.py

--- spot.py
+++ spot.py
@@ -2,14 +2,14 @@ from telethon.sync import TelegramClient
 from telethon.tl import functions, types
 import time
 
-api_id = '21202416'
-api_hash = 'b70404d0fe911e638a187c81d3a8a28f'
-phone_number = '+79151768207'
+api_id = '0000'
+api_hash = '0000'
+phone_number = '0000'
 
 with TelegramClient('session_name', api_id, api_hash) as client:
     while True:
         # Отправка сообщения в чат
-        chat_id = '5633724545'
+        chat_id = '0000'
         message = '🧀 Получить карту'
         client.send_message(chat_id, message)
         
